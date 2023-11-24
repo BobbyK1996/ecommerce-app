ad4602ebff32914a761c5a74c783b9c89e2e045e
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-24 00:25:22 +0000

    Moved bodyParser to app.use()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send(`
     <div>
@@ -37,7 +39,7 @@ app.get("/", (req, res) => {
 //   }
 // };
 
-app.post("/", bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
   res.send("Account created!");
 });
