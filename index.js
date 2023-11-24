5c56b040e55fd9721d00f6b7fcac13de3d8689b7
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-24 00:16:30 +0000

    Took the logic for parsing post data and moved it into a middleware function to be re-used

--- index.js
+++ index.js
@@ -19,18 +19,27 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", (req, res) => {
-  //get access to email, password and passwordConfirmation
-  req.on("data", (data) => {
-    const parsed = data.toString("utf8").split("&");
-    const formData = {};
-    for (let pair of parsed) {
-      const [key, value] = pair.split("=");
-      formData[key] = value;
-    }
+const bodyParser = (req, res, next) => {
+  if (req.method === "POST") {
+    req.on("data", (data) => {
+      const parsed = data.toString("utf8").split("&");
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split("=");
+        formData[key] = value;
+      }
 
-    console.log(formData);
-  });
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post("/", bodyParser, (req, res) => {
+  //get access to email, password and passwordConfirmation
+  console.log(req.body);
   res.send("Account created!");
 });
 
