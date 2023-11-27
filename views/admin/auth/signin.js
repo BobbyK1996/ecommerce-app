47475eea987f0295aab7158325a381e56ba7baec
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-27 10:39:43 +0000

    Created a layout.js to create a default html layout for the html served by signup and signin. This means that the browser does not have to fix the html I send to it

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+import layout from "../layout.js";
+
 export default () => {
-  return `
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
+    `,
+  });
 };
