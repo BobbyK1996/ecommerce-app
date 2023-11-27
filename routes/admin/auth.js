0322f9780f27ee5b5c93bcd7bd1ef6c57b6efbc7
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-27 10:03:20 +0000

    Moved routing post and get requests to an auth.js file. Created a subrouter and linked it back to app in index.js

---
+++ routes/admin/auth.js
@@ -0,0 +1,96 @@
+import express from "express";
+import usersRepo from "../../repositories/users.js";
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  res.send(`
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input
+          name="passwordConfirmation"
+          placeholder="password confirmation"
+        />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+// const bodyParser = (req, res, next) => {
+//   if (req.method === "POST") {
+//     req.on("data", (data) => {
+//       const parsed = data.toString("utf8").split("&");
+//       const formData = {};
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split("=");
+//         formData[key] = value;
+//       }
+
+//       req.body = formData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// };
+
+router.post("/signup", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
+  const user = await usersRepo.create({ email, password });
+  req.session.userId = user.id;
+
+  res.send("Account created!");
+});
+
+router.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out");
+});
+
+router.get("/signin", (req, res) => {
+  res.send(`<div>
+    <form method="POST">
+      <input name="email" placeholder="email" />
+      <input name="password" placeholder="password" />
+      <button>Sign In</button>
+    </form>
+  </div>`);
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send("Email not found");
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+  if (!validPassword) {
+    return res.send("Invalid Password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are signed in");
+});
+
+export default router;
