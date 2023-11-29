cbf5d631df42caf16dd1a13a857d914fa46cf168
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-29 16:42:54 +0000

    Created a products folder in views/admin with a new.js inside of it, which contained html mockup for the new items page. Called it in router.get for admin/products/new

---
+++ views/admin/products/new.js
@@ -0,0 +1,17 @@
+import layout from "../layout.js";
+import helpers from "../../helpers.js";
+
+const { getError } = helpers;
+
+export default ({ errors }) => {
+  return layout({
+    content: `
+      <form method="POST">
+        <input placeholder="Title" name="title" />
+        <input placeholder="Price" name="price" />
+        <input type="file" name="price" />
+        <button>Submit</button>
+      </form>
+    `,
+  });
+};
