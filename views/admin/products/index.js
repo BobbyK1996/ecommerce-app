1bde596d680ec4cce760dd37a541e0fb5e9724be
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-30 23:15:43 +0000

    Realised that my commit messages are too long and not concise, so will try to shorten moving forward. Added html markup for index.js. Changed incorrectly linked css file in layout.js

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -3,14 +3,46 @@ import layout from "../layout.js";
 export default ({ products }) => {
   const renderedProducts = products
     .map((product) => {
-      return `<div>${product.title}</div>`;
+      const formattedPrice = product.price.toLocaleString("en-GB", {
+        style: "currency",
+        currency: "GBP",
+      });
+
+      return ` 
+      <tr>
+        <td>${product.title}</td>
+        <td>${formattedPrice}</td>
+        <td>
+          <a href="">
+            <button class="button is-link">Edit</button>
+          </a>
+        </td>
+        <td>
+          <button class="button is-danger">Delete</button>
+        </td>
+      </tr>`;
     })
     .join("");
 
   return layout({
     content: `
-      <h1 class="title">Products</h1>
-      ${renderedProducts}
+      <div class="control">
+        <h1 class="subtitle">Products</h1>
+        <a href="/admin/products/new" class="button is-primary">New Product</a>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderedProducts}
+        </tbody>
+      </table>
     `,
   });
 };
