91d2578c406de5e847d4c3b3d531da6d029c6053
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-11-30 16:22:06 +0000

    Created middleware that redirects the user to the sign in page if the user has not signed in and is trying to access admin/products or admin/products/new. If the user has signed in and has a session userId in their cookie, they are allowed to access the pages.

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 
-import { handleErrors } from "./middlewares.js";
+import { handleErrors, requireAuth } from "./middlewares.js";
 import productsRepo from "../../repositories/products.js";
 import productsNewTemplate from "../../views/admin/products/new.js";
 import productsIndexTemplate from "../../views/admin/products/index.js";
@@ -11,17 +11,18 @@ const { requireTitle, requirePrice } = validationRulesProducts;
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
