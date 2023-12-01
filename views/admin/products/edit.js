6f16e73e5ecf857395fc07d5eb8806360b702dd2
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-12-01 00:42:46 +0000

    There was an error with handleErrors where I usually pass through an error, but for .post edit I passed in a product. Rewrote handleErrors to accept an optional second parameter called dataCallback that finds a product and expands it into object parameter for the function created by templateFunc

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -1,11 +1,14 @@
 import layout from "../layout.js";
+import { getError } from "../../helpers.js";
 
-export default ({ product }) => {
+export default ({ product, errors }) => {
   return layout({
     content: `
       <form method="POST">
         <input name="title" value="${product.title}" />
+        ${getError(errors, "title")}
         <input name="price" value="${product.price}" />
+        ${getError(errors, "price")}
         <input name="image" type="file" />
         <button>Submit</button>
       </form>
