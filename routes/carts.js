83310ffc8d3f665db8a30bd312fabcffa0693a8c
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-12-02 23:12:24 +0000

    Added the ability to add items cart, storing only the id and quantity of the item in carts.json

--- routes/carts.js
+++ routes/carts.js
@@ -14,7 +14,20 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
   //Either increment quantity of an existing product, or add a new product altogether.
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  console.log(existingItem);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send("Product added to cart");
 });
